0d7befe50468b66fd7ef79c0ef01c7f7c290cc82
Jimmy Bergström <me@kwarf.com>
2026-01-04 19:46:21 +0100

    Add shikane for output profiles

--- homes/x86_64-linux/kwarf/default.nix
+++ homes/x86_64-linux/kwarf/default.nix
@@ -146,6 +146,36 @@
   };
 
   services = {
+    shikane = {
+      enable = true;
+      settings = {
+        profile = [
+          {
+            name = "builtin-only";
+            output = [
+              {
+                match = "eDP-1";
+                enable = true;
+              }
+            ];
+          }
+          {
+            name = "docked";
+            output = [
+              {
+                match = "eDP-1";
+                enable = false;
+              }
+              {
+                match = "m=LF32TU87";
+                enable = true;
+              }
+            ];
+          }
+        ];
+      };
+    };
+
     wob = {
       enable = true;
     };
