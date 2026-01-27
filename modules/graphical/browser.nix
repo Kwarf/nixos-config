ced91eca73fd9b589c054bfc4e6d9e548056bd68
Jimmy Bergström <me@kwarf.com>
2026-01-27 23:22:19 +0100

    browser: remove firefox profile
    I'll sync everything with a Mozilla account instead.

--- modules/graphical/browser.nix
+++ modules/graphical/browser.nix
@@ -1,48 +1,5 @@
 {
   flake.modules.homeManager.graphical = {
-    programs.firefox = {
-      enable = true;
-      policies = {
-        AutofillCreditCardEnabled = false;
-        DisableFirefoxStudies = true;
-        DisablePocket = true;
-        DisableTelemetry = true;
-        EnableTrackingProtection = {
-          Value = true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-        };
-        ExtensionSettings = {
-          "*".installation_mode = "blocked";
-          # uBlock Origin
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Proton Pass
-          "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
-            installation_mode = "force_installed";
-          };
-        };
-        FirefoxHome = {
-          Search = true;
-          Pocket = false;
-          Snippets = false;
-          TopSites = false;
-          Highlights = false;
-        };
-        OfferToSaveLogins = false;
-        OfferToSaveLoginsDefault = false;
-        OverrideFirstRunPage = "";
-        OverridePostUpdatePage = "";
-        PasswordManagerEnabled = false;
-        UserMessaging = {
-          ExtensionRecommendations = false;
-          SkipOnboarding = true;
-        };
-      };
-    };
+    programs.firefox.enable = true;
   };
 }
