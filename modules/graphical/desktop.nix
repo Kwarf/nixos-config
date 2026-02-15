d09d9003d39956d09ebcd0fffeca047dbc52a4f8
Jimmy Bergström <me@kwarf.com>
2026-02-15 22:16:44 +0100

    desktop: configure swayidle

--- modules/graphical/desktop.nix
+++ modules/graphical/desktop.nix
@@ -16,11 +16,29 @@
   flake.modules.homeManager.graphical =
     { config, pkgs, ... }:
     let
+      cmd-lock = "${pkgs.swaylock}/bin/swaylock -fF";
+      cmd-display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
       graphical = "${config.home.homeDirectory}/nixos-config/modules/graphical";
     in
     {
       programs.swaylock.enable = true;
       catppuccin.swaylock.enable = true;
+
+      services.swayidle = {
+        enable = true;
+        systemdTarget = "niri.service";
+        events = {
+          after-resume = cmd-display "on";
+          before-sleep = cmd-lock;
+          lock = cmd-lock;
+        };
+        timeouts = [
+          {
+            timeout = 300;
+            command = cmd-lock + "; " + (cmd-display "off");
+          }
+        ];
+      };
 
       home.packages = with pkgs; [
         wbg
