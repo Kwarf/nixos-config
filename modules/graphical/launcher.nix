99e9508e57ab252d96cd9324cb17698ae83017e3
Jimmy Bergström <me@kwarf.com>
2026-02-22 22:58:38 +0100

    launcher: theming

--- modules/graphical/launcher.nix
+++ modules/graphical/launcher.nix
@@ -4,10 +4,19 @@
       enable = true;
       systemd.enable = true;
       settings = {
+        launcher_window = {
+          opacity = 1.0;
+          client_side_decorations = {
+            rounding = 0;
+            border_width = 4;
+          };
+        };
+        favorites = [ ];
         providers = {
           clipboard.preferences.monitoring = false;
         };
       };
     };
+    catppuccin.vicinae.enable = true;
   };
 }
