518411f951db6a67d102e42e2470accb10e89f3b
Jimmy Bergström <me@kwarf.com>
2026-03-29 23:46:17 +0200

    gnome: add tiling-shell extension

--- modules/software/gnome.nix
+++ modules/software/gnome.nix
@@ -19,33 +19,42 @@
         ];
       };
 
-    homeManager = {
-      # Run "dconf watch /" then make the desired change to see how to define it
-      dconf.settings = {
-        "org/gnome/shell" = {
-          favorite-apps = [
-            "org.gnome.Nautilus.desktop"
-            "firefox.desktop"
-            "org.gnome.Console.desktop"
+    homeManager =
+      { pkgs, ... }:
+      {
+        programs.gnome-shell = {
+          enable = true;
+          extensions = [
+            { package = pkgs.gnomeExtensions.tiling-shell; }
           ];
         };
-        "org/gnome/desktop/interface" = {
-          font-antialiasing = "rgba";
-        };
-        "org/gnome/desktop/wm/preferences" = {
-          mouse-button-modifier = "<Alt>";
-          resize-with-right-button = true;
-        };
-        "org/gnome/settings-daemon/plugins/housekeeping" = {
-          donation-reminder-enabled = false;
-        };
-        "org/gnome/desktop/peripherals/touchpad" = {
-          natural-scroll = false;
-        };
-        "org/gnome/desktop/peripherals/pointingstick" = {
-          speed = -0.6;
+
+        # Run "dconf watch /" then make the desired change to see how to define it
+        dconf.settings = {
+          "org/gnome/shell" = {
+            favorite-apps = [
+              "org.gnome.Nautilus.desktop"
+              "firefox.desktop"
+              "org.gnome.Console.desktop"
+            ];
+          };
+          "org/gnome/desktop/interface" = {
+            font-antialiasing = "rgba";
+          };
+          "org/gnome/desktop/wm/preferences" = {
+            mouse-button-modifier = "<Alt>";
+            resize-with-right-button = true;
+          };
+          "org/gnome/settings-daemon/plugins/housekeeping" = {
+            donation-reminder-enabled = false;
+          };
+          "org/gnome/desktop/peripherals/touchpad" = {
+            natural-scroll = false;
+          };
+          "org/gnome/desktop/peripherals/pointingstick" = {
+            speed = -0.6;
+          };
         };
       };
-    };
   };
 }
