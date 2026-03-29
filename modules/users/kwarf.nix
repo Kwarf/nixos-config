00544a29e6c1e1d0366a0448d9499a620923cef1
Jimmy Bergström <me@kwarf.com>
2026-03-29 22:58:50 +0200

    kwarf: update nyx ssh key

--- modules/users/kwarf.nix
+++ modules/users/kwarf.nix
@@ -13,7 +13,7 @@ in
       hashedPassword = secrets.hashedUserPassword;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgyUhHbmhbQtQ62dYG35WjSdxErYNTnG9dbexV46l4P kwarf@icarus"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMJGAZco2yp8m42ShyTe9m7GRf5sD7myEUJYFf0jKith kwarf@nyx"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1uNk/boWIBtPpsBs/shTkriyEpWwXjorwwb63Cea2M kwarf@nyx"
       ];
     };
 
