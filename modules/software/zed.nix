fc47c2a77b56ec7451b36562a79fe83d14c1e2fa
Jimmy Bergström <me@kwarf.com>
2026-03-30 02:08:35 +0200

    zed: ported dotfiles settings

--- modules/software/zed.nix
+++ modules/software/zed.nix
@@ -12,6 +12,32 @@
             nixd
             nixfmt
           ];
+          userSettings = {
+            buffer_font_family = "Adwaita Mono";
+            buffer_font_size = 14;
+            ui_font_family = "Adwaita Sans";
+            ui_font_size = 16;
+            wrap_guides = [ 120 ];
+            show_whitespaces = "boundary";
+            tab_bar = {
+              show_pinned_tabs_in_separate_row = true;
+              show_nav_history_buttons = false;
+            };
+            tabs = {
+              git_status = true;
+            };
+            minimap = {
+              show = "auto";
+            };
+            collaboration_panel = {
+              button = false;
+            };
+            auto_update = false;
+            telemetry = {
+              diagnostics = false;
+              metrics = false;
+            };
+          };
         };
       };
   };
