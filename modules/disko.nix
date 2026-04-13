74ea2ed3cffd7a7ed92b610841f4381bf07f27d6
Jimmy Bergström <me@kwarf.com>
2026-04-13 17:37:06 +0200

    disko: remove problematic code
    I blindly copied this from another config on GitHub (yeah, I know..) and
    it just now started causing evaluation errors. Disko seems to still work
    perfectly fine without it..? So I just removed it.

--- modules/disko.nix
+++ modules/disko.nix
@@ -1,29 +1,11 @@
-{
-  den,
-  lib,
-  inputs,
-  ...
-}:
-let
-  fwd =
-    { host }:
-    den.provides.forward {
-      each = lib.singleton host;
-      fromClass = _: "disko";
-      intoClass = _: host.class;
-      intoPath = _: [ "disko" ];
-      guard = { options, ... }: options ? disko;
-      fromAspect = _: den.aspects.${host.aspect};
-    };
-in
+{ inputs, ... }:
 {
   flake-file.inputs.disko = {
     url = "github:nix-community/disko";
     inputs.nixpkgs.follows = "nixpkgs";
   };
+
   den.ctx.host.includes = [
-    fwd
     { nixos.imports = [ inputs.disko.nixosModules.disko ]; }
   ];
-  den.schema.host.classes = lib.mkDefault [ "disko" ];
 }
